f762c3174bf1f1eac229093b1fb55266655aa7be
ThomasVanGheluwe98 <tvangheluwe@hotmail.com>
2022-10-04 13:39:03 +0200

    ls1

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+      return {
+        product: { title: 'Socks', description: 'A nice pair of socks' },
+      };
+    },
+  });
+  
+  
